640535c589b532651e0ac392c12a2b00fddc5af7
Student <Student@III-EEIT>
2015-06-30 16:43:38 +0800

    hi

--- proj09/src/proj09/Abc.java
+++ proj09/src/proj09/Abc.java
@@ -5,6 +5,8 @@ public class Abc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hellow World");
+		System.out.println("Hellow World");
+		System.out.println("Hellow World");
 	}
 
 }
